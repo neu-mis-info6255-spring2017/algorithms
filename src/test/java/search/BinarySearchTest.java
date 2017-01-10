22ed850c1abb5aa73fddf818fb9989c54561d512
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2017-01-10 05:21:15 -0500

    Adding display name for tests

--- src/test/java/search/BinarySearchTest.java
+++ src/test/java/search/BinarySearchTest.java
@@ -6,8 +6,10 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Binary Search Tests")
 public class BinarySearchTest {
 	private int[] values;
 	
@@ -22,12 +24,14 @@ public class BinarySearchTest {
 	}
 	
 	@Test
+	@DisplayName("Test to find a value before mid")
 	public void findValueBeforeMid() {
 		int index = Arrays.binarySearch(values, 6);
 		assertEquals(2, index, "Value index did not match");
 	}
 	
 	@Test
+	@DisplayName("Test to check the insertion point of not found value")
 	public void checkInsertPointOfNotFoundValue() {
 		int index = Arrays.binarySearch(values, 20);
 		assertEquals(-8, index, "Insertion point is not correct");
