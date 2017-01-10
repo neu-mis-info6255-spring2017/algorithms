394d2d49d9817800255654e5cc7506e3d27c0a17
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2017-01-10 04:13:11 -0500

    Adding binary search test

---
+++ src/test/java/search/BinarySearchTest.java
@@ -0,0 +1,14 @@
+package search;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+
+	@Test
+	public void test() {
+		assertEquals(true, true);
+	}
+
+}
