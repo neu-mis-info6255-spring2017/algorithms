fed1a603bb041e3812192c5ba9d66e73d6141815
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2017-01-10 05:18:18 -0500

    Adding binary search tests

--- src/test/java/search/BinarySearchTest.java
+++ src/test/java/search/BinarySearchTest.java
@@ -1,14 +1,36 @@
 package search;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BinarySearchTest {
-
+	private int[] values;
+	
+	@BeforeEach
+	public void setUp() {
+		values = new int[] {2,3,6,9,10,13,19,34};
+	}
+	
+	@AfterEach
+	public void tearDown() {
+		values = null;
+	}
+	
 	@Test
-	public void test() {
-		assertEquals(true, true);
+	public void findValueBeforeMid() {
+		int index = Arrays.binarySearch(values, 6);
+		assertEquals(2, index, "Value index did not match");
+	}
+	
+	@Test
+	public void checkInsertPointOfNotFoundValue() {
+		int index = Arrays.binarySearch(values, 20);
+		assertEquals(-8, index, "Insertion point is not correct");
 	}
 
 }
